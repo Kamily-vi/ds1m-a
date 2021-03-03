cd0895c1c5b090d182b447fa177fb0963c14eab6
Kamily <Kamilytosta.kv@gmail.com>
2021-03-03 11:20:42 -0300

    aula 03-03-2021 - Turma A

---
+++ ConversorCelsiusParaFahrenheit/src/ConversorCelsiusParaFahrenheit.java
@@ -0,0 +1,16 @@
+
+public class ConversorCelsiusParaFahrenheit {
+
+	public static void main(String[] args) {
+	
+		float graus;
+		float fahrenheit;
+		
+		graus = 27;
+				
+		fahrenheit = (9*27+160)/5f;
+		
+		System.out.print(graus + " graus celsius equiavale a " + fahrenheit + " graus fahrenheit");
+	}
+
+}
---
+++ ConversorCelsiusParaFahrenheit/src/celsius.java
@@ -0,0 +1,16 @@
+
+public class ConversorCelsiusParafahrenheit {
+
+		public static void main(String[] args) {
+			
+			float graus;
+			float fahrenheit;
+			
+			graus = 27;
+					
+			fahrenheit = (9*27+160)/5f;
+		
+			System.out.print(fahrenheit);
+			
+		}
+}
